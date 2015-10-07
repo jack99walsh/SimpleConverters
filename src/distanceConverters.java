a0c733c769e58813ec61846677267b363c8c026c
Jack Walsh <jack99walsh@gmail.com>
2015-10-07 13:53:28 -0400

    Removed a class file

--- src/distanceConverters.java
+++ src/distanceConverters.java
@@ -11,41 +11,41 @@ public class distanceConverters {
 
         double measurement = Double.parseDouble(measurementIn); //Parsed the measurement to be used in future variables as a double
 
-        double inchToCentimeter = measurement * 2.54;
-        double inchToFoot = measurement / 12;				//From inches conversions
-        double inchToYard = measurement / 36;
+        final double inchToCentimeter = measurement * 2.54;
+        final double inchToFoot = measurement / 12;				//From inches conversions
+        final double inchToYard = measurement / 36;
 
-        double footToInch = measurement * 12;
-        double footToYard = measurement / 3;
-        double footToMile = measurement / 5280;				//From feet conversions
-        double footToMeter = measurement / 0.3048;
-        double footToKilometer = measurement / 0.0003048;
+        final double footToInch = measurement * 12;
+        final double footToYard = measurement / 3;
+        final double footToMile = measurement / 5280;				//From feet conversions
+        final double footToMeter = measurement / 0.3048;
+        final double footToKilometer = measurement / 0.0003048;
 
-        double yardToFoot = measurement / 3;
-        double yardToInch = measurement / 36;				//From yards conversions
-        double yardToMeter = measurement * .9144;
+        final double yardToFoot = measurement / 3;
+        final double yardToInch = measurement / 36;				//From yards conversions
+        final double yardToMeter = measurement * .9144;
 
-        double millimeterToInch = measurement * 0.03937;	//From millimeters conversions
+        final double millimeterToInch = measurement * 0.03937;	//From millimeters conversions
 
-        double centimeterToMillimeter = measurement * 10;
-        double centimeterToInch = measurement * 0.3937;		//From centimeters conversions
-        double centimeterToMeter = measurement / 100;
-        double centimeterToKilometer = measurement / 100000;
+        final double centimeterToMillimeter = measurement * 10;
+        final double centimeterToInch = measurement * 0.3937;		//From centimeters conversions
+        final double centimeterToMeter = measurement / 100;
+        final double centimeterToKilometer = measurement / 100000;
 
-        double meterToMillimeter = measurement * 1000;
-        double meterToCentimeter = measurement * 100;
-        double meterToKilometer = measurement / 1000;		//From meter Conversions
-        double meterToFoot = measurement * 3.28084;
-        double meterToYard = measurement * 1.0936;
+        final double meterToMillimeter = measurement * 1000;
+        final double meterToCentimeter = measurement * 100;
+        final double meterToKilometer = measurement / 1000;		//From meter Conversions
+        final double meterToFoot = measurement * 3.28084;
+        final double meterToYard = measurement * 1.0936;
 
-        double kilometerToCentimeter = measurement * 100000;
-        double kilometerToMeter = measurement * 1000;
-        double kilometerToMile = measurement * .6214;		//From kilometers conversions
-        double kilometerToFoot = measurement * 3280.84;
+        final double kilometerToCentimeter = measurement * 100000;
+        final double kilometerToMeter = measurement * 1000;
+        final double kilometerToMile = measurement * .6214;		//From kilometers conversions
+        final double kilometerToFoot = measurement * 3280.84;
 
-        double mileToKilometer = measurement * 1.60934;
-        double mileToYard = measurement * 1760;				//From Miles Conversions
-        double mileToFoot = measurement * 5280;
+        final double mileToKilometer = measurement * 1.60934;
+        final double mileToYard = measurement * 1760;				//From Miles Conversions
+        final double mileToFoot = measurement * 5280;
 
 		/*Everything below is used for the output. It takes the Input Data and allows it to be output as the correct conversion.
 		This all hinges on the converting variables above, as well as the inputs from the JOptionPanes. Do not alter unless needed.*/
